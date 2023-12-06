b7c1a6fb0c74ac5f0e9c9dcb254f36a2b03ad410
Gowrav Vishwakarma <gowravvishwkarma@gmail.com>
2023-12-06 18:21:22 +0530

    Moved Actions outside or models

--- src/dto/ActionListDTO.ts
+++ src/dto/ActionListDTO.ts
@@ -1,68 +1,68 @@
 import {
-  IsString,
-  IsNotEmpty,
-  ValidateNested,
-  IsObject,
-  IsArray,
-  IsOptional,
-} from "class-validator";
-import { Type } from "class-transformer";
+    IsString,
+    IsNotEmpty,
+    ValidateNested,
+    IsObject,
+    IsArray,
+    IsOptional,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
 class UIOptionsDTO {
-  @IsString()
-  mode: string;
+    @IsString()
+    mode: string;
 
-  @IsNotEmpty()
-  title: string;
+    @IsNotEmpty()
+    title: string;
 
-  @IsNotEmpty()
-  message: string;
+    @IsNotEmpty()
+    message: string;
 
-  @IsNotEmpty()
-  okLabel: string;
+    @IsNotEmpty()
+    okLabel: string;
 
-  @IsNotEmpty()
-  cancelLabel: string;
+    @IsNotEmpty()
+    cancelLabel: string;
 }
 
 export class ActionListDTO {
-  @IsNotEmpty()
-  name: string;
+    @IsNotEmpty()
+    name: string;
 
-  @IsNotEmpty()
-  label: string;
+    @IsNotEmpty()
+    label: string;
 
-  @IsNotEmpty()
-  icon: string;
+    @IsNotEmpty()
+    icon: string;
 
-  @IsString()
-  @IsOptional()
-  iconColor?: string;
+    @IsString()
+    @IsOptional()
+    iconColor?: string;
 
-  @IsNotEmpty()
-  description: string;
+    @IsNotEmpty()
+    description: string;
 
-  @IsNotEmpty()
-  mode: string;
+    @IsNotEmpty()
+    mode: string;
 
-  @IsObject()
-  @ValidateNested()
-  @Type(() => UIOptionsDTO)
-  ui: UIOptionsDTO;
+    @IsObject()
+    @ValidateNested()
+    @Type(() => UIOptionsDTO)
+    ui: UIOptionsDTO;
 
-  @IsString()
-  @IsOptional()
-  loadBy?: string;
+    @IsString()
+    @IsOptional()
+    loadBy?: string;
 
-  @IsArray()
-  @IsString({ each: true })
-  roles: string[];
+    @IsArray()
+    @IsString({ each: true })
+    roles: string[];
 
-  @IsObject()
-  @IsOptional()
-  condition?: Record<string, any>;
+    @IsObject()
+    @IsOptional()
+    condition?: Record<string, any>;
 
-  @IsObject()
-  @IsOptional()
-  returnModel: Record<string, any> | boolean = false;
+    @IsObject()
+    @IsOptional()
+    returnModel: Record<string, any> | boolean = false;
 }
--- src/qnatk.module.ts
+++ src/qnatk.module.ts
@@ -1,57 +1,69 @@
-import { DynamicModule, Module, Global, Inject } from "@nestjs/common";
-import { ModuleRef } from "@nestjs/core";
-// import { SequelizeModule } from "@nestjs/sequelize";
-import { QnatkModuleOptions } from "./hooks/qnatk-module.options";
-import { HooksService } from "./hooks/hooks.service";
-import { AutoRegisterHooks } from "./hooks/auto-register.hook";
-import { QnatkService } from "./qnatk.service";
-import { QnatkControllerService } from "./qnatk-controller.service";
+import { DynamicModule, Module, Global, Inject } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ModuleRef } from '@nestjs/core';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { QnatkModuleOptions } from './hooks/qnatk-module.options';
+import { HooksService } from './hooks/hooks.service';
+import { AutoRegisterHooks } from './hooks/auto-register.hook';
+import { QnatkService } from './qnatk.service';
+import { QnatkControllerService } from './qnatk-controller.service';
+import { ActionListDTO } from './dto/ActionListDTO';
 
 @Global()
 @Module({
-  imports: [],
-  providers: [],
-  exports: [],
+    imports: [],
+    providers: [],
+    exports: [],
 })
 export class QnatkModule {
-  static forRoot(
-    options: QnatkModuleOptions,
-    modelArray: any[] = [],
-    additionalImports: any[] = [],
-    additionalProviders: any[] = []
-  ): DynamicModule {
-    return {
-      module: QnatkModule,
-      imports: [
-        // EventEmitterModule.forRoot(),
-        // SequelizeModule.forFeature(modelArray),
-        ...additionalImports,
-      ],
-      providers: [
-        {
-          provide: "HOOKS_OPTIONS",
-          useValue: options,
+    static forRoot(
+        options: QnatkModuleOptions,
+        modelAndActions: {
+            models: any[];
+            actions: Record<string, ActionListDTO[]>;
+        } = {
+            models: [],
+            actions: {},
         },
-        HooksService,
-        QnatkService,
-        QnatkControllerService,
-        ...additionalProviders,
-      ],
-      exports: [HooksService, QnatkControllerService],
-    };
-  }
+        additionalImports: any[] = [],
+        additionalProviders: any[] = [],
+    ): DynamicModule {
+        return {
+            module: QnatkModule,
+            imports: [
+                EventEmitterModule.forRoot(),
+                SequelizeModule.forFeature(modelAndActions.models),
+                ...additionalImports,
+            ],
+            providers: [
+                {
+                    provide: 'HOOKS_OPTIONS',
+                    useValue: options,
+                },
+                {
+                    provide: 'MODEL_ACTIONS',
+                    useValue: modelAndActions.actions,
+                },
+                HooksService,
+                QnatkService,
+                QnatkControllerService,
+                ...additionalProviders,
+            ],
+            exports: [HooksService, EventEmitterModule, QnatkControllerService],
+        };
+    }
 
-  constructor(
-    private moduleRef: ModuleRef,
-    private hooksService: HooksService,
-    @Inject("HOOKS_OPTIONS") private options: QnatkModuleOptions
-  ) {}
+    constructor(
+        private moduleRef: ModuleRef,
+        private hooksService: HooksService,
+        @Inject('HOOKS_OPTIONS') private options: QnatkModuleOptions,
+    ) {}
 
-  async onModuleInit() {
-    await AutoRegisterHooks(
-      this.moduleRef,
-      this.hooksService,
-      this.options.hooksDirectory
-    );
-  }
+    async onModuleInit() {
+        await AutoRegisterHooks(
+            this.moduleRef,
+            this.hooksService,
+            this.options.hooksDirectory,
+        );
+    }
 }
