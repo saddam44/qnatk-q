d7df0296a06181fc23db10c7f8a27283dbadfb2b
Gowrav Vishwakarma <gowravvishwkarma@gmail.com>
2023-12-12 11:14:04 +0530

    Added BaseHook

---
+++ src/hooks/base-hook.ts
@@ -0,0 +1,46 @@
+import { Transaction } from 'sequelize';
+import { HookInterface } from './hook.interface';
+import { validateOrReject, ValidationError } from 'class-validator';
+import { ValidationException } from '../Exceptions/ValidationException';
+import { plainToInstance } from 'class-transformer';
+
+export abstract class BaseHook implements HookInterface {
+    priority: number = 0;
+
+    // Define the `execute` method as abstract so that derived classes must implement it.
+    abstract execute(
+        previousData: any,
+        originalData: any,
+        transaction: Transaction | undefined,
+        extraInfo?: any,
+        passedExtraData?: Record<string, any>,
+    ): Promise<any>;
+
+    // Method to validate data against a DTO and return the DTO object
+    async validateData<T extends object>(
+        data: T,
+        DTOClass: new () => T,
+    ): Promise<T> {
+        const dtoInstance = plainToInstance(DTOClass, data);
+        try {
+            await validateOrReject(dtoInstance);
+            return dtoInstance;
+        } catch (errors) {
+            throw new ValidationException(
+                this.mapValidationErrors(errors as ValidationError[]),
+            );
+        }
+    }
+
+    mapValidationErrors(
+        validationErrors: ValidationError[],
+    ): Record<string, string[]> {
+        return validationErrors.reduce((acc, err) => {
+            const constraints = err.constraints;
+            acc[err.property] = constraints
+                ? [Object.values(constraints).join('. ')]
+                : ['An error occurred'];
+            return acc;
+        }, {});
+    }
+}
