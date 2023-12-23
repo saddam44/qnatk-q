59e3b4c8d2129bd77fa5a0aa244dd286f05d4c2c
Gowrav Vishwakarma <gowravvishwkarma@gmail.com>
2023-12-23 11:03:05 +0530

    non so required params sent to last and optional now

--- src/hooks/base-hook.ts
+++ src/hooks/base-hook.ts
@@ -10,17 +10,14 @@ export abstract class BaseHook implements HookInterface {
     // Define the `execute` method as abstract so that derived classes must implement it.
     abstract execute(
         previousData: any,
-        originalData: any,
         transaction: Transaction | undefined,
+        originalData: any,
         extraInfo?: any,
         passedExtraData?: Record<string, any>,
     ): Promise<any>;
 
     // Method to validate data against a DTO and return the DTO object
-    async validateData<T extends object>(
-        data: T,
-        DTOClass: new () => T,
-    ): Promise<T> {
+    async validateData<T extends object>(data: T, DTOClass: new () => T): Promise<T> {
         let dtoInstance;
 
         // Check if data is already an instance of DTOClass
@@ -36,20 +33,14 @@ export abstract class BaseHook implements HookInterface {
             return dtoInstance;
         } catch (errors) {
             console.log(errors);
-            throw new ValidationException(
-                this.mapValidationErrors(errors as ValidationError[]),
-            );
+            throw new ValidationException(this.mapValidationErrors(errors as ValidationError[]));
         }
     }
 
-    mapValidationErrors(
-        validationErrors: ValidationError[],
-    ): Record<string, string[]> {
+    mapValidationErrors(validationErrors: ValidationError[]): Record<string, string[]> {
         return validationErrors.reduce((acc, err) => {
             const constraints = err.constraints;
-            acc[err.property] = constraints
-                ? [Object.values(constraints).join('. ')]
-                : ['An error occurred'];
+            acc[err.property] = constraints ? [Object.values(constraints).join('. ')] : ['An error occurred'];
             return acc;
         }, {});
     }
